19f2d664364e57ecaa5849bed1b6d1b14dddabb9
Pavan Gottavada <gottavadapavan@gmail.com>
2026-03-21 17:13:24 +0530

    anagram

--- String/Anagrams.java
+++ String/Anagrams.java
@@ -9,13 +9,24 @@ public class Anagrams {
 
 		String str1 = "listen";
 		String str2 = "silent";
+		
+		// Convert to lowercase to ignore case sensitivity
+		str1 = str1.toLowerCase();
+		str2 = str2.toLowerCase();
+
+		// Remove spaces (for phrases like "listen" & "silent")
+		str1 = str1.replaceAll("\\s", "");
+		str2 = str2.replaceAll("\\s", "");
  		
+		// convert to char array
 		char[] chars1 = str1.toCharArray();
 		char[] chars2 = str2.toCharArray();
 		
+		// sort
 		Arrays.sort(chars1);
 		Arrays.sort(chars2);
-
+		
+		
 		if(Arrays.equals(chars1, chars2)) {
 			System.out.print("anagram");
 		}else {
